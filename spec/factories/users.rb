137abeffd69b6441e919078cead64c8cc57b73a1
Ken-Nakayama <katana1028@gmail.com>
2024-01-24 19:15:06 +0900

    単体テスト_userモデル

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {"ニックネーム"}
+    email                 {Faker::Internet.email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    last_name             {"ミョウジ"}
+    first_name            {"名前"}
+    last_name_kana        {"ミョウジ"}
+    first_name_kana       {"ナマエ"}
+    birth_date            {"1981-10-28"}
+  end
+end
