c61db31e67162edd80d4f708e5fe8284093aecd9
Ken-Nakayama <katana1028@gmail.com>
2024-01-25 19:47:14 +0900

    ActiveHash_category

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,6 +8,13 @@ class Item < ApplicationRecord
     validates :birth_date
   end
 
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  belongs_to :category
+
+
   # has_many :items
   # has_many :orders
   has_one_attached :image
