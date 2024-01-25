9d906c5b39b032f760a007eded53df9b73308bed
Ken-Nakayama <katana1028@gmail.com>
2024-01-25 20:16:08 +0900

    ActiveHash全定義

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,11 +9,18 @@ class Item < ApplicationRecord
   end
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :sales_status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_fee_status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :scheduled_delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
 
   extend ActiveHash::Associations::ActiveRecordExtensions
 
   belongs_to :category
-
+  belongs_to :sales_status
+  belongs_to :shipping_fee_status
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
 
   # has_many :items
   # has_many :orders
