cc5d69f15bfaf85411fd53b163d2b1751abe5908
Ken-Nakayama <katana1028@gmail.com>
2024-01-28 22:09:12 +0900

    Revert "商品情報編集機能実装"
    This reverts commit abd21fb0e84e21d8d7d0326064e16b464dd595b1.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit, :update]
+  before_action :authenticate_user!, only: [:new]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -23,27 +23,7 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  def edit
-    @item = Item.find(params[:id])
-    redirect_to root_path unless can_edit_item?(@item)
-  end
-
-  def update
-    @item = Item.find(params[:id])
-    redirect_to root_path unless can_edit_item?(@item)
-    if @item.update(item_params)
-      redirect_to item_path(@item)
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
-
   private
-
-  def can_edit_item?(item)
-    current_user && current_user.id == item.user_id
-  end
 
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
