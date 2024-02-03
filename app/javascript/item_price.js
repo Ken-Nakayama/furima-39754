259ed4f160dd0306afdee79275884d501424c7f8
Ken-Nakayama <katana1028@gmail.com>
2024-02-03 17:46:41 +0900

    挙動確認修正２

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,9 +1,5 @@
-document.addEventListener('turbo:load', () => {
-
-const priceInput = document.getElementById("item-price");
-
-priceInput.addEventListener("input", () => {
-
+function updatePrice() {
+  const priceInput = document.getElementById("item-price");
   const inputValue = priceInput.value;
 
   const addTaxDom = document.getElementById("add-tax-price");
@@ -11,6 +7,17 @@ priceInput.addEventListener("input", () => {
 
   const addProfitDom = document.getElementById("profit");
   addProfitDom.innerHTML = Math.floor(inputValue * 0.9);
-})
+}
 
+document.addEventListener('turbo:load', () => {
+  updatePrice();
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", updatePrice);
 });
+
+document.addEventListener('turbo:render', () => {
+    updatePrice();
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", updatePrice);
+});
+
